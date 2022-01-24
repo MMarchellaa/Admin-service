e8740d2654bd11ccb2da346997f85a9463984156
MMarchellaa <mark.mihalkovi@mail.ru>
2022-01-24 17:05:25 +0300

    DEV-5: Create and setup repositories

---
+++ src/main/java/com/mihalkovich/adminservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mihalkovich.adminservice.repository;
+
+import com.mihalkovich.adminservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
